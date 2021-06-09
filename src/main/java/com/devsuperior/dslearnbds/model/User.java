4c1ceb8272a9e08f9dd2a99c1e114bd10cdbfb3f
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-06-09 13:23:52 -0300

    Model User e Role

---
+++ src/main/java/com/devsuperior/dslearnbds/model/User.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dslearnbds.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
